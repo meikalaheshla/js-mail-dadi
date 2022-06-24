cdd67ae1003941166f8acf66dc5b560eed7042a9
meikalaheshla <fausto.boshoff@gmail.com>
2022-06-24 16:24:19 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// Mail
+// Crea (inventa) una lista di inidirizzi mail autorizzati.
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa in console un messaggio appropriato sull’esito del controllo. ("Benvenuto" se la mail è nella lista, "Spiacenti" se non lo è);
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilisci il vincitore, in base a chi fa il punteggio più alto e stampa  in console il risultato (ossia se ha vinto il computer o il player)
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Che ci sia un array da qualche parte?
+// Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+// Consigli del giorno:
+// 1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+// 2. javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
+// 3. si ma noi cosa vogliamo fare?
+// 4. torniamo a scrivere in italiano
+// 5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+// BONUS:
+// Svolgere entrambi gli esercizi con la stessa logica ma lavorare con il DOM per stampare i risultati.
+// Provare a prendere la mail dell'utente da un input e fare la verifica dopo aver cliccato su un bottone
+// CSS a piacimento
+
+
+
+
+
