b39eadff4b68bc9d10d59b562f14527d71a388c6
meikalaheshla <fausto.boshoff@gmail.com>
2022-06-24 16:40:55 +0200

    BONUS FOLDER ADDED

---
+++ BONUS/js/bonusscript.js
@@ -0,0 +1,3 @@
+// Svolgere entrambi gli esercizi con la stessa logica ma lavorare con il DOM per stampare i risultati.
+// Provare a prendere la mail dell'utente da un input e fare la verifica dopo aver cliccato su un bottone
+// CSS a piacimento
